55273b6d73f00152feb1a1a5efbd706c2669648b
Pranav garg <121023702+22104050@users.noreply.github.com>
2026-09-09 13:43:46 +0530

    Use Pandas DataFrame for displaying fruit options
    Converted Snowflake DataFrame to Pandas DataFrame for compatibility with Streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests  
-
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -17,8 +17,11 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
